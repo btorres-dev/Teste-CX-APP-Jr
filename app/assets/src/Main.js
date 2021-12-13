2775bdbd54ffcbb280fa9c69a161fe9b71a9e926
Breno Torres <brenotorres.br@gmail.com>
2021-12-13 01:52:17 -0300

    feat: adicionado formulário com campo assunto

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -9,7 +9,19 @@ client.metadata().then((metadata) => {
 
 const Main = async () => {
   const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+  let appBody = `
+  <div id="main-content">
+    <form id="subjectForm"> 
+      <div>
+        <h2> Alterar assunto para o formato padrão (assunto + data e hora)</h2>
+        <input id="subject" type="text" value="${subject}" type="text" placeholder="assunto + data e hora"/>
+        <button type="submit">Alterar<button>
+      </div>
+    </form>
+    <div>
+      <h3>Listar ultimos tickets</h3>
+    </div>
+  </div>`;
 
   // Write App
   App.innerHTML = appBody;
